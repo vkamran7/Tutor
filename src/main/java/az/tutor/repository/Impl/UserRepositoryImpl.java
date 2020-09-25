ca82d303e850ad98c22be0724b9802c3b636b4b0
kamran <kamran.valizada@mdm.gov.az>
2020-09-25 18:31:08 +0400

    Good Commit

--- src/main/java/az/tutor/repository/Impl/UserRepositoryImpl.java
+++ src/main/java/az/tutor/repository/Impl/UserRepositoryImpl.java
@@ -5,6 +5,8 @@ import az.tutor.model.Role;
 import az.tutor.model.User;
 import az.tutor.repository.UserRepository;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 
@@ -15,13 +17,17 @@ import java.util.Optional;
 @Log4j2
 public class UserRepositoryImpl implements UserRepository {
 
+    private final JdbcTemplate jdbcTemplate;
 
+    public UserRepositoryImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
     public void addUser(User user) {
         try {
             boolean success = true;
-
+            KeyHolder keyHolder;
 
         } catch (Exception ex) {
             log.error(ErrorMessages.ERROR_REGISTER + user, ex);
